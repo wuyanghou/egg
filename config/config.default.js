06323ff17f8c88bcca52722e949ba4af3d898f43
luoming <1661391985.com>
2020-05-19 11:10:02 +0800

    //

--- config/config.default.js
+++ config/config.default.js
@@ -28,24 +28,24 @@ module.exports = appInfo => {
     jwt: {
       secret: '123456',
     },
-    mysql: {
-      client: {
-        // host
-        host: '127.0.0.1',
-        // 端口号
-        port: '3306',
-        // 用户名
-        user: 'root',
-        // 密码
-        password: 'TYlm920606',
-        // 数据库名
-        database: 'egg-test',
-      },
-      // 是否加载到 app 上，默认开启
-      app: true,
-      // 是否加载到 agent 上，默认关闭
-      agent: false,
-    },
+    // mysql: {
+    //   client: {
+    //     // host
+    //     host: '127.0.0.1',
+    //     // 端口号
+    //     port: '3306',
+    //     // 用户名
+    //     user: 'root',
+    //     // 密码
+    //     password: 'TYlm920606',
+    //     // 数据库名
+    //     database: 'egg-test',
+    //   },
+    //   // 是否加载到 app 上，默认开启
+    //   app: true,
+    //   // 是否加载到 agent 上，默认关闭
+    //   agent: false,
+    // },
   };
 
   config.cluster = {
@@ -55,14 +55,14 @@ module.exports = appInfo => {
     },
   };
 
-  config.sequelize = {
-    dialect: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    database: 'egg-test',
-    username: 'root',
-    password: 'TYlm920606',
-  };
+  // config.sequelize = {
+  //   dialect: 'mysql',
+  //   host: '127.0.0.1',
+  //   port: 3306,
+  //   database: 'egg-test',
+  //   username: 'root',
+  //   password: 'TYlm920606',
+  // };
 
   config.swaggerdoc = {
     dirScanner: './app/controller',
