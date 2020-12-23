c73b24227f08d424404af405941d5429914158ba
luoming <1661391985.com>
2020-12-24 00:03:43 +0800

    增加新浪财经数据

---
+++ app/schedule/email.js
@@ -0,0 +1,34 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: 'qq',
+  secureConnection: true,
+  port: 465,
+  auth: {
+    user: '178647142@qq.com', // 账号
+    pass: 'dnlnxedoahxcbhfi', // SMTP授权码
+  },
+});
+
+
+module.exports = {
+  schedule: {
+    interval: '1m', // 1 分钟间隔
+    // cron: '0 0 9 * * ?', // 每天9点触发 https://www.cnblogs.com/javahr/p/8318728.html
+    type: 'worker', // 指定所有的 worker 都需要执行
+  },
+  async task(ctx) {
+    console.log('emailemail');
+    const mailOptions = {
+      from: '178647142@qq.com', // 发送者,与上面的user一致
+      to: '1661391985@qq.com', // 接收者,可以同时发送多个,以逗号隔开
+      subject: '今天又是元气满满的一天哦', // 标题
+      html: '<h2>加油，打工人！</h2>',
+    };
+    transporter.sendMail(mailOptions).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
+  },
+};
---
+++ app/schedule/xinlang.js
@@ -0,0 +1,35 @@
+'use strict';
+const cheerio = require('cheerio');
+
+module.exports = {
+  schedule: {
+    interval: '1m', // 1 分钟间隔
+    // cron: '0 * 10,14 * * ?', // 10-14:59 每分钟执行一次 https://www.cnblogs.com/javahr/p/8318728.html
+    type: 'worker', // 指定所有的 worker 都需要执行
+  },
+  async task(ctx) {
+    try{
+      const result = await ctx.curl('https://hq.sinajs.cn/rn=1608731604516&list=s_sh603025');
+      const time = new Date();
+      const h = time.getHours();
+      const m = time.getMinutes();
+      const s = time.getSeconds();
+      const timeStr = `时间：${h}:${m}:${s}`;
+      console.log(timeStr, 'timeStr');
+      const list = result.data.toString().split('=');
+      let d= list[1].replace(/\\n/g, '');
+      d= d.replace(';', '').replace('"', '').replace('"', '');
+      const data = d.split(',');
+      const title = `大豪科技${Number(data[1])},涨跌价${Number(data[2])},涨跌幅${Number(data[3])}`
+      await ctx.curl('https://sc.ftqq.com/SCU117835Tf59f2299a43d58f3a208f85d8c1240b95f83f10332d72.send', {
+        dataType: 'json',
+        data: {
+          text: `${title}`,
+          desp: '股票提醒',
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
